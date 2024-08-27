ad48d4df676c35ba9d265d69d217af0eb84b2f5d
hamidb80 <hr.bolouri@gmail.com>
2024-08-27 22:34:50 +0330

    can save tasks

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function uuid() {
   return self.crypto.randomUUID()
 }
 
-function reJSON(obj) {
+function purify(obj) {
   return JSON.parse(JSON.stringify(obj))
 }
 
@@ -46,7 +46,7 @@ function newAction(name, emoji, boost, desc) {
 }
 
 function newTask(id, name, actions, max, decrate) {
-  return { id, name, actions, max, decrate }
+  return { id, name, actions, max, decrate, archived: false }
 }
 
 function newRecord(taskID, time, boost, desc) {
@@ -82,25 +82,31 @@ var tasks = undefined
 var selectedTask = undefined
 
 function getTasks() {
-  return getItemDB(TasksK) ?? []
+  return getItemDB(TasksK) ?? {}
 }
+
+function saveTasks(tasksMap) {
+  return setItemDB(TasksK, tasksMap)
+}
+
+function saveTask(task) {
+  tasks[task.id] = task
+  saveTasks(tasks)
+}
+
 
 function initGlobalsIfNot() {
   if (tasks === undefined)
     tasks = getTasks()
-
-  if (selectedTask === undefined)
-    selectedTask = {
-      actions: [
-        {
-          name: "wow"
-        }
-      ]
-    }
-
 }
 
 // ------ UI 
+
+rivets.formatters['not'] = b => !b
+rivets.formatters['task_url'] = task => {
+  let suburl = q`#new-task-link`.getAttribute('href')
+  return suburl + '?task-id=' + task.id
+}
 
 up.macro('main', initGlobalsIfNot)
 
@@ -113,21 +119,46 @@ up.macro('[smooth-link]', link => {
 })
 
 up.compiler('#app-page', el => {
-  rivets.bind(el, { tasks })
+  let all = Object.values(tasks)
+  let actives = all.filter(t => !t.archived)
+  let archives = all.filter(t => t.archived)
+  rivets.bind(el, {
+    active_tasks: actives,
+    archived_tasks: archives
+  })
 })
 
 up.compiler('#task-settings-page', el => {
+  let qp = new URLSearchParams(window.location.search)
+  let taskid = qp.get('task-id')
+  selectedTask = taskid ? purify(tasks[taskid]) : newTask(uuid(), '', [], '', '')
+
+  rivets.binders['task-settings-remove-action-click'] = (el, index) => {
+    el.onclick = () => {
+      selectedTask.actions.splice(index, 1)
+    }
+  }
   rivets.bind(el, { task: selectedTask })
+})
+
+up.compiler('#task-settings-archive', el => {
+  el.onclick = () => {
+    selectedTask.archived = true
+    console.log(selectedTask)
+  }
 })
 
 up.compiler('#task-settings-apply', el => {
   el.onclick = () => {
-    console.log(reJSON(selectedTask))
+    saveTask(selectedTask)
+    console.log('saved!') // TODO notif
   }
 })
 
 up.compiler('#task-settings-add-action', el => {
   el.onclick = () => {
-    selectedTask.actions.push(newAction('', '', 0, ''))
+    selectedTask.actions.push(newAction('', '', '', ''))
   }
 })
+
+// TODO HTML validate
