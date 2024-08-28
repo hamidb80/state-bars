4bd098c7e542d444c6837ea2b1652403cf7731c3
hamidb80 <hr.bolouri@gmail.com>
2024-08-28 06:42:52 +0330

    works

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 // -------- Other Utils -----------------
 
-function unow() {
+function p2(smth) {
+  return String(smth).padStart(2, '0')
+}
+
+function unixToFormattedDate(unixTimestamp) {
+  let oo = new Date(unixTimestamp * 1000) // Convert to milliseconds
+  let yyyy = oo.getFullYear()
+  let mm = p2(oo.getMonth() + 1)
+  let dd = p2(oo.getDate())
+  return `${yyyy}-${mm}-${dd}`
+}
+
+function unixToFormattedTime(unixTimestamp) {
+  let oo = new Date(unixTimestamp * 1000) // Convert to milliseconds
+  let hh = p2(oo.getHours())
+  let tt = p2(oo.getMinutes())
+  let ss = p2(oo.getSeconds())
+  return `${hh}:${tt}:${ss}`
+}
+
+function unixNow() {
   return Math.floor(Date.now() / 1000)
+}
+
+function sec2hour(secs) {
+  return secs / (60 * 60)
 }
 
 function toArray(arrayLike) {
   return Array.from(arrayLike)
+}
+
+function obj2array(obj) {
+  return Object.values(obj)
 }
 
 function uuid() {
@@ -16,6 +44,22 @@ function purify(obj) {
   return JSON.parse(JSON.stringify(obj))
 }
 
+function shallowCopy(smth) {
+  if (Array.isArray(smth)) return [...smth]
+  if (typeof smth === "object") return { ...smth }
+  else return smth
+}
+
+function filterchop(arr, pred) {
+  var does = []
+  var donts = []
+
+  for (let item of arr) {
+    if (pred(item)) does.push(item)
+    else donts.push(item)
+  }
+  return [does, donts]
+}
 
 // -------- DataBase Utils ---------------
 
@@ -60,9 +104,18 @@ function newRecord(time, boost) {
 // ------ Domain Logic
 
 function computeState(task, records) {
-  task.created
-  task.decrate
-  return 10
+  records.push(newRecord(unixNow(), 0))
+
+  var timecurr = task.created
+  var boostlevel = 0
+
+  for (let [rtime, boost] of records) {
+    let decay = task.decrate * sec2hour(rtime - timecurr)
+    boostlevel = Math.min(task.max, Math.max(0, boostlevel - decay) + boost)
+    timecurr = rtime
+  }
+
+  return boostlevel
 }
 
 // ------ DOM Utils
@@ -80,7 +133,6 @@ function setAttrs(el, attrsObj) {
     el.setAttribute(key, attrsObj[key])
 }
 
-
 // ------- Setup ------------------------------
 
 // ------ Globals 
@@ -88,6 +140,7 @@ function setAttrs(el, attrsObj) {
 var tasks = undefined
 var selectedTask = undefined
 var records = undefined
+var selectedRecords = undefined
 
 
 function getAllTasks() {
@@ -109,11 +162,15 @@ function saveTask(task) {
   tasks[task.id] = task
   return saveAllTasks(tasks)
 }
-function getRecordFor(taskid) {
+function getRecordsFor(taskid) {
   return records[taskid] ?? []
 }
-function saveRecordFor(taskid, time, boost) {
-  let recs = getRecordFor(taskid)
+function saveRecordsFor(taskid, recs) {
+  records[taskid] = recs
+  return saveAllRecords(records)
+}
+function addSaveRecordFor(taskid, time, boost) {
+  let recs = getRecordsFor(taskid)
   recs.push(newRecord(time, boost))
   records[taskid] = recs
   return saveAllRecords(records)
@@ -124,30 +181,61 @@ function initGlobalsIfNot() {
   records = getAllRecords()
 }
 
+// ------ Helpers ?
+
+function taskidFromUrl() {
+  return new URLSearchParams(window.location.search).get('task-id')
+}
+
+function taskElemId(task) {
+  return "task-" + task.id
+}
+
 // ------ UI 
 
+function setProgressBar(taskid) {
+  let task = tasks[taskid]
+  let pbar = q(`#${taskElemId(task)} .progress-bar`)
+  let percent = 100 * computeState(task, shallowCopy(getRecordsFor(task.id))) / task.max
+  pbar.style.width = percent + '%'
+}
+
 rivets.formatters['not'] = b => !b
+rivets.formatters['task_element_id'] = taskElemId
 rivets.formatters['task_settings_url'] = task => {
   let suburl = q`#new-task-link`.getAttribute('href')
   return suburl + '?task-id=' + task.id
 }
 rivets.formatters['task_stats_url'] = task => {
-  let suburl = q`#tast-stats-base`.getAttribute('href')
+  let suburl = q`#test-stats-base`.getAttribute('href')
   return suburl + '?task-id=' + task.id
 }
+rivets.formatters['reversed'] = arr => arr.slice().reverse()
+
+rivets.formatters['unixtime_to_formatted_date'] = ut => unixToFormattedDate(parseInt(ut))
+rivets.formatters['unixtime_to_formatted_time'] = ut => unixToFormattedTime(parseInt(ut))
+
 rivets.binders['task-settings-remove-action-click'] = (el, index) => {
   el.onclick = () => {
     selectedTask.actions.splice(index, 1)
   }
 }
-rivets.binders['click-action-btn'] = (el, boost) => {
+rivets.binders['task-stats-remove-record-click'] = (el, index) => {
   el.onclick = () => {
-    saveRecordFor(
-      el.getAttribute('task-id'),
-      unow(),
-      parseInt(boost))
+    selectedRecords.splice(index, 1)
   }
 }
+
+rivets.binders['click-action-btn'] = (el, boost) => {
+  let taskid = el.getAttribute('task-id')
+
+  el.onclick = () => {
+    addSaveRecordFor(taskid, unixNow(), parseInt(boost))
+    setProgressBar(taskid)
+  }
+  setTimeout(() => setProgressBar(taskid), 0)
+}
+
 
 up.macro('main', initGlobalsIfNot)
 
@@ -159,33 +247,27 @@ up.macro('[smooth-link]', link => {
   })
 })
 
-up.compiler('#app-page', el => {
-  let all = Object.values(tasks)
-  let actives = all
-    .filter(t => !t.archived)
-    .map(task => ({
-      ...task,
-      state: computeState(task, getRecordFor(task.id))
-    }))
-  let archives = all.filter(t => t.archived)
 
-  rivets.bind(el, {
-    active_tasks: actives,
-    archived_tasks: archives
-  })
+up.compiler('#app-page', el => {
+  let [archived_tasks, active_tasks] = filterchop(obj2array(tasks), t => t.archived)
+  rivets.bind(el, { active_tasks, archived_tasks })
+})
+
+up.compiler('#task-stats-page', el => {
+  let task = tasks[taskidFromUrl()]
+  selectedRecords = getRecordsFor(task.id)
+  rivets.bind(el, { records: selectedRecords, task })
 })
 
 up.compiler('#task-settings-page', el => {
-  let qp = new URLSearchParams(window.location.search)
-  let taskid = qp.get('task-id')
-  selectedTask = taskid ? purify(tasks[taskid]) : newTask(uuid(), '', [], '', '', unow())
+  let taskid = taskidFromUrl()
+  selectedTask = taskid ? purify(tasks[taskid]) : newTask(uuid(), '', [], '', '', unixNow())
   rivets.bind(el, { task: selectedTask })
 })
 
 up.compiler('#task-settings-archive', el => {
   el.onclick = () => {
     selectedTask.archived = true
-    console.log(selectedTask)
   }
 })
 
@@ -206,6 +288,12 @@ up.compiler('#task-settings-delete', el => {
 up.compiler('#task-settings-add-action', el => {
   el.onclick = () => {
     selectedTask.actions.push(newAction('', '', '', ''))
+  }
+})
+
+up.compiler('#tasks-stats-apply-btn', el => {
+  el.onclick = () => {
+    saveRecordsFor(taskidFromUrl(), selectedRecords)
   }
 })
 
